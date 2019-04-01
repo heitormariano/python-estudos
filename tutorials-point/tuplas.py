a60413cbb6b2a1ca566522b05539e848225c1e08
Heitor Camara <heitor.eumesmo@gmail.com>
2019-04-01 10:18:32 -0300

    adding expplication abou tuples and lists

--- tutorials-point/tuplas.py
+++ tutorials-point/tuplas.py
@@ -2,7 +2,7 @@
 
 # Diferença básica entre listas e tuplas
 # Listas - elementos da lista ficam entre "[]" e podem ser atualizados
-# Tuplas - elementos da lista ficam entre "()" e NÂO podem ser atualizados (a tupla não pode ser modificada)
+# Tuplas - elementos da tupla ficam entre "()" e NÂO podem ser atualizados (a tupla não pode ser modificada)
 
 tupla = ('Massa', 1, 'Show', 2, 'Perfeito', 458.2)
 lista = ['Legal', 10, 'Bacana', 50, 'Uhuuu', 12.6]
